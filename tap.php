457994ca96810af7f319e102c0a2565455713ce1
sean lyons <sean@hexault.com>
2014-01-12 21:45:51 -0600

    reconciled a few idiosyncracies in tests, colorized output, added check() to validate

--- tap.php
+++ tap.php
@@ -7,35 +7,46 @@ class Tap {
 	protected $counter = 0;
 	protected $passed = 0;
 	protected $failed = 0;
-
+    protected $debug = false;
+    
 	function __destruct() {
 		list($counter, $passed, $failed) = array($this->counter, $this->passed, $this->failed);
 		echo "\n$counter tests run: $passed passed, $failed failed.\n\n";
 	}
 
-	function is($msg, $class_name, $function_name, $input, $expected_output) {
+	function is($msg, $class_name, $function_name, $expected_output = NULL, $input = NULL) {
+        if ($this->debug === TRUE) { print_r(array($msg, $class_name, $function_name, $expected_output, $input)); }
 		if ($this->checkClassAndMethod($class_name, $function_name) === FALSE) {
 			return;
 		}
-		
 		$class = $this->retClass( $class_name );
-		$returned = $class::$function_name();
+		$instance = new $class;
+		$returned = $instance->$function_name( $input );
+
+        if ( ! is_scalar($expected_output)) {
+            $expected_output = json_encode($expected_output);
+        }
+        if ( ! is_scalar($returned)) {
+            $returned = json_encode($returned);
+        }
 		if ($returned == $expected_output) {
-			$outcome = "matched on `$returned`.\n";
+			$outcome = SUCCESS . "matched on `$returned`." . NL ;
 			$this->passed++;
 		} else {
-			$outcome = "Unmatched scalar: Expected `$expected_output`; received `$returned`.\n";
+			$outcome = FAILURE . "Unmatched comparison: Expected `$expected_output`; received `$returned`." . NL;
 			$this->failed++;
 		}
-		echo '#' . ++$this->counter . " $msg: $outcome\n";
+		echo '#' . ++$this->counter . " $msg: $outcome";
 	}
 	
-	function is_instance_of($msg, $class_name, $function_name, $input, $expected_output) {
+	function is_instance_of($msg, $class_name, $function_name, $expected_output = NULL, $input = NULL) {
 		if ($this->checkClassAndMethod($class_name, $function_name) === FALSE) {
 			return;
 		}
+        
 		$class = $this->retClass( $class_name );
-		$returned = $class::$function_name();
+		$instance = new $class;
+		$returned = $instance->$function_name();        
 		
 		if ($returned instanceof $expected_output) {
 			$outcome = SUCCESS . "instanceof matched on `$expected_output." . NL;
@@ -48,27 +59,52 @@ class Tap {
 		echo '#' . ++$this->counter . " $msg: $outcome";
 	}	
 	
-	function is_resource_type($msg, $class_name, $function_name, $input, $expected_resource_type) {
+	function is_resource_type($msg, $class_name, $function_name, $expected_resource_type = NULL, $input = NULL) {
 		if ($this->checkClassAndMethod($class_name, $function_name) === FALSE) {
 			return;
 		}
-		
 		$class = $this->retClass( $class_name );
-		$returned = $class::$function_name();
+		$instance = new $class;
+		$returned = $instance->$function_name();
+        
 		$returned = get_resource_type( $returned );
 		if ($returned === FALSE) {
-			$outcome = "Error: returned value not a resource.\n";
+			$outcome = FAILURE . "Error: returned value not a resource." . NL;
 			$this->failed++;
 		} elseif ($returned == $expected_resource_type) {
-			$outcome = "matched resource type on`$returned`.\n";
+			$outcome = SUCCESS . "matched resource type on`$returned`." . NL;
 			$this->passed++;
 		} else {
-			$outcome = "Unmatched resource type: Expected `$expected_resource_type`; received `$returned`.\n";
+			$outcome = FAILURE . "Unmatched resource type: Expected `$expected_resource_type`; received `$returned`." . NL;
 			$this->failed++;
 		}
 		echo '#' . ++$this->counter . " $msg: $outcome\n";
 		print_r($returned);		
 	}
+	
+	function check($msg, $class_name, $function_name, $expected_output = NULL, $input = NULL) {
+		if ($this->checkClassAndMethod($class_name, $function_name) === FALSE) {
+			return;
+		}
+		$class = $this->retClass( $class_name );
+		$instance = new $class;
+		$returned = $instance->$function_name();
+		
+		if (is_scalar($returned)) {
+			//It's fine to output with a simple echo.
+		} elseif (is_array($returned)) {
+			$returned = json_encode($returned);
+		} elseif (is_object($returned)) {
+			$returned = get_class($returned);
+		} elseif (is_resource($returned)) {
+			$returned = get_resource_type( $returned );
+		}		
+		
+		echo "#CHECKING: $msg: \n\t> $returned\n";
+	}
+
+#######################################################################################################################################	
+	
 	
 	function retClass( $class_name ) {
 		return new $class_name;	
